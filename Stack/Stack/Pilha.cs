71032cdb35bc7906a9bfd311caeb0d4d2ffda4fb
VictorBwD <victor.bogdanowdornelles@gmail.com>
2022-01-04 14:03:17 -0300

    [ADDED] added project Pilha (Stack)

---
+++ Stack/Stack/Pilha.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    class Pilha
+    {
+        Posicao primeiro;
+        public void Empilha(object item)
+        {
+            primeiro = new Posicao(primeiro, item);
+        }
+
+        public object Desempilha()
+        {
+            if (primeiro == null)
+            {
+                throw new InvalidOperationException("A pilha está vazia!");
+            }
+
+            object resultado = primeiro.item;
+            primeiro = primeiro.proximo;
+            return resultado;
+        }
+
+        class Posicao
+        {
+            public Posicao proximo;
+            public object item;
+            public Posicao(Posicao proximo, object item)
+            {
+                this.proximo = proximo;
+                this.item = item;
+            }
+        }
+    }
+}
