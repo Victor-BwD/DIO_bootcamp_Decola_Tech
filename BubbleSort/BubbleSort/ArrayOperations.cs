a28393ce7d584b639e0734483a4eb81387b1d288
VictorBwD <victor.bogdanowdornelles@gmail.com>
2022-01-06 14:46:48 -0300

    [ADDED] Added project bubblesort

---
+++ BubbleSort/BubbleSort/ArrayOperations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BubbleSort
+{
+    class ArrayOperations
+    {
+        public void BubbleSort(ref int[] array)
+        {
+            int aux = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if(array[j] > array[j + 1])
+                    {
+                        aux = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = aux;
+                    }
+                }
+            }
+        }
+
+        public void ShowArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array);
+            }
+        }
+    }
+}
---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayOperations op = new ArrayOperations();
+            int[] array = new int[5] { 6, 4, 8, 9, 3 };
+
+            op.ShowArray(array);
+
+            op.BubbleSort(ref array);
+
+            Console.WriteLine("Ordered array:");
+            op.ShowArray(array);
+        }
+    }
+}
